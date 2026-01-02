727484bc27b9aa7dc0a573a1e1237a7a371bf3e5
Eranga <erangakavishanka@icloud.com>
2026-01-02 23:58:33 +0530

    imported

--- src/main/java/com/devstack/ci/pipeline/controller/TestController.java
+++ src/main/java/com/devstack/ci/pipeline/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.devstack.ci.pipeline.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/tests")
 public class TestController {
+  
   
 }
