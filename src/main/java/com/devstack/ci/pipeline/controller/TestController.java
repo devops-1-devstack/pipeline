0be45d4d20d665f9c30fcda9279427819c34e66c
Eranga <erangakavishanka@icloud.com>
2026-01-02 23:59:18 +0530

    developed

--- src/main/java/com/devstack/ci/pipeline/controller/TestController.java
+++ src/main/java/com/devstack/ci/pipeline/controller/TestController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/tests")
 public class TestController {
+
+  public String test(){
+    return " ";
+  }
   
   
 }
